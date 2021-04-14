9013dcbd8c344702e02ca2ac7bf5fa66351548a7
theIanMilan <theIanMilan@users.noreply.github.com>
2021-04-14 20:24:15 +0800

    Add JS on video container

--- Avion Projects/Product Landing Page/script.js
+++ Avion Projects/Product Landing Page/script.js
@@ -1,7 +1,9 @@
 // // testing JavaScript
 
-// var shopButtons = document.querySelectorAll(".button-shop");
-// for (let i = 0; i < shopButtons.length; i++) {
-//     shopButtons[i].style.backgroundColor = "goldenrod";
-// }
+var shopButtons = document.querySelectorAll(".button-shop");
+for (let i = 0; i < shopButtons.length; i++) {
+    shopButtons[i].style.backgroundColor = "goldenrod";
+}
 
+var roastContainer = document.getElementById("how-we-roast-container")
+roastContainer.style.backgroundColor = "linen";
