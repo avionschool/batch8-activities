9cd2f8d9576b57bc3b7cc1c18b34197592caec11
theIanMilan <theIanMilan@users.noreply.github.com>
2021-04-14 19:19:28 +0800

    Test JavaScript

---
+++ Avion Projects/Product Landing Page/script.js
@@ -0,0 +1,6 @@
+// testing JavaScript
+
+var shopButtons = document.querySelectorAll(".button-shop");
+for (let i = 0; i < shopButtons.length; i++) {
+    shopButtons[i].style.backgroundColor = "goldenrod";
+}
