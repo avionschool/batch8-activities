7e53a648d2a8b20052c4ac6cea77b1d2c18d6ce7
theIanMilan <theIanMilan@users.noreply.github.com>
2021-04-19 23:23:36 +0800

    Add quote hover

--- Avion Projects/Momentum (App)/script.js
+++ Avion Projects/Momentum (App)/script.js
@@ -2,7 +2,6 @@
 var quotesList = [
     `“We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein`,
     `“Learn as if you will live forever, live like you will die tomorrow.” — Mahatma Gandhi`,
-    `“Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.” — Mark Twain`,
     `“When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.” — Eleanor Roosevelt`,
     `“When you change your thoughts, remember to also change your world.” — Norman Vincent Peale`,
     `“It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.” — Walter Anderson`,
@@ -26,12 +25,6 @@ var quotesList = [
     // Append to HTML
     docQuoteText.textContent = quoteText;
     docQuoteAuthor.textContent = quoteAuthor;
-
-    // Add mouseenter event to Quote-Text [TO-DO]
-    docQuoteText.addEventListener('mouseenter', () => {
-        docQuoteAuthor.style.display = "initial";
-        docQuoteText.setAttribute('style', 'position: absolute; left: 0; right: 0');
-    })
 
     function generateRandomQuote() {
         var index = Math.floor(Math.random()* (quotesList.length));
