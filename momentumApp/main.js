b600b3808b2ae06fccd9eded46c3add7b29d7de9
Edwin Sacalamitao <edwin@avionschool.com>
2021-04-20 22:54:01 +0800

    time

---
+++ momentumApp/main.js
@@ -0,0 +1,26 @@
+// var today = new Date ();
+// var time = today.getHours () + ":" + today.getMinutes ();
+// document.getElementById("time").value = time;
+
+function currentTime() {
+    var date = new Date(); /* creating object of Date class */
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("time").value = hour + " : " + min + " : " + sec; /* adding time to the div */
+      var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+  }
+  
+  function updateTime(k) {
+    if (k < 10) {
+      return "0" + k;
+    }
+    else {
+      return k;
+    }
+  }
+  
+  currentTime(); /* calling currentTime() function to initiate the process */
