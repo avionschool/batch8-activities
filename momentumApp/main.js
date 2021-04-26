4ccc597570548ea5ccfb8c208f60310f34cb4656
Edwin Sacalamitao <edwin@avionschool.com>
2021-04-26 19:14:17 +0800

    temporary - main to be uploaded

--- momentumApp/main.js
+++ momentumApp/main.js
@@ -34,14 +34,43 @@ function currentTime() {
   currentTime(); /* calling currentTime() function to initiate the process */
 
 
+// const date = new Date();
+// const hrs = date.getHours();
+// const mn = date.getMinutes();
+// const local = date.toLocaleTimeString([], { timeStyle: "short" });
+// document.getElementById("time").innerHTML = local;
+
+// var t = setTimeout(function(){ currentTime() }, 1000);
+
+  // function updateTime(k) {
+  //   if (k < 10) {
+  //     return "0" + k;
+  //   }
+  //   else {
+  //     return k;
+  //   }
+  // }
+
+  // currentTime();
+
+
+
   function greetings1(){
     var name = prompt ("Enter your Name here.");
+    // var name = document.getElementById("greeting").style.visibility = "hidden";
+    // var focus = document.getElementById("mainFocus").style.visibility = "hidden";
+
+    // inputName.addEventListener("click", function(){
+    //   name.style.visibility = "visible";
+    //   focus.style.visibility = "visible";
+    // })
+    
 
     if(currentTime <= 12) {
-      var greeting1 = "Good Morning ";
+      var greeting1 = "Good Evening, ";
     }
     else {
-      var greeting1 ="Good Evening ";
+      var greeting1 ="Good Morning, ";
     }
 
     if (name != null) {
@@ -50,3 +79,50 @@ function currentTime() {
   }
 
   greetings1();
+
+  // todo list
+
+  let addToDoButton = document.getElementById("addToDo");
+  let toDoContainer = document.getElementById("toDoContainer");
+  let inputField = document.getElementById("inputField");
+
+  addToDoButton.addEventListener("click", function(){
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener("click", function(){
+      paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener("dblclick", function(){
+      toDoContainer.removeChild(paragraph);
+    })
+  })
+
+  const todoInput = document.getElementById("inputField");
+
+  todoInput.addEventListener("keyup", function(event) {
+    if(event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("addToDo").click();
+    }
+  })
+
+// edit from here
+
+var modal = document.getElementById("myModal")
+var btn = document.getElementById("addToDo");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
