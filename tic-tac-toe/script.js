c4f58a52802ea311c1e89bfe96c5f3d0536b95b2
lea927 <lxthym@icloud.com>
2021-05-05 21:42:28 +0800

    Added nextMove function

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -16,7 +16,9 @@ const winningCombo = [
   [2, 4, 6]
 ];
 let xArr = [0, 1, 2];
-let historyArr,prevMoveArr = [];
+let prevMoveArr = [];
+let nextMoveArr = [];
+let historyArr = [];
 
 // triggers when an individual cell gets clicked
 const cellClicked = (e) => {
@@ -25,17 +27,52 @@ const cellClicked = (e) => {
   updatePlayerText();
   e.target.textContent = currentPlayer;
   prevMoveArr.push(cellId);
-  // historyArr.push(cellId);
-  console.log(prevMoveArr);
+  historyArr.push(cellId);
 };
 
 // displays previous move
 prevMove = () => {
+  // gets index of previous move array's last item
   let lastItem = prevMoveArr[prevMoveArr.length-1]
+
+  // removes last item from previous move array 
+  // Syntax: (index,howMany) -1 index for last item
   prevMoveArr.splice(-1,1);
-  // console.log(document.getElementById(`${lastItem}`));
-  document.getElementById(`${lastItem}`).innerText = null;
-  console.log(prevMoveArr); 
+
+  // empties the content of individual based on which was the last item of previous array
+  document.getElementById(`${lastItem}`).textContent = null;
+  nextMoveArr.push(lastItem);
+  console.log("prevMoveArr:" + prevMoveArr);
+};
+
+// displays next move
+
+nextMove = () => {
+  // console.log("history: " + historyArr);
+
+  // let nextItem = prevMoveArr.length;
+
+  // nextItem ++;
+  // for-loop to iterate items of history array
+  // for (let i = nextItem ; i < historyArr.length; i++) {
+    // const element = array[index];
+    // console.log(nextItem);
+    
+  // }
+
+  // console.log(historyArr[nextItem]);
+  // document.getElementById(`${nextItem}`).textContent = "Z";
+  console.log("nextMoveArr:" + nextMoveArr);
+ 
+  // get last item of next move array
+  let nextItem = nextMoveArr[nextMoveArr.length-1];
+  console.log(nextItem);
+
+  nextMoveArr.splice(-1,1);
+  console.log(currentPlayer);
+  document.getElementById(`${nextItem}`).textContent = currentPlayer;
+  switchPlayer();
+
 };
 
 // switches X to O, vice-versa then updates value of variable currentPlayer
@@ -47,7 +84,7 @@ checkWinner = () => {
 
 };
 
-// updates who's currently playing - dependent on switchPlayer function
+// updates who's currently playing
 updatePlayerText = () => {
   currentPlayer === O ? nextPlayer = X : nextPlayer = 0;
   playerText.textContent = `${nextPlayer}'s turn`
