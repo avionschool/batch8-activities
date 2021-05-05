4d6a6bb143223338a69e9f19ba7d5bb85a4746b1
jhrs <66736876+JansonSiy@users.noreply.github.com>
2021-05-05 09:15:30 -0400

    Updated

---
+++ Activity_9_Tic_Tac_Toe/A9.js
@@ -0,0 +1,17 @@
+let board;
+
+let playerOne = 'X';
+let playerTwo = 'O';
+
+let winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+let tBox = document.querySelectorAll('.tBox')
