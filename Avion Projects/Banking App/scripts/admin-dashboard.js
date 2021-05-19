d2da63a1b05ec6c00cf61141270f8dc2882a8382
theIanMilan <theIanMilan@users.noreply.github.com>
2021-05-19 20:19:44 +0800

    Complete content and add navigation to page

--- Avion Projects/Banking App/scripts/admin-dashboard.js
+++ Avion Projects/Banking App/scripts/admin-dashboard.js
@@ -1,3 +1,7 @@
+// ===================
+// == A. Nav Bar   ===
+// ===================
+
 const navItems = [...document.querySelectorAll('.nav-item')];
 const navHome = document.querySelector('.nav-home');
 const navUserList = document.querySelector('.nav-user-list');
@@ -26,6 +30,50 @@ function clearNavItem() {
 
 function hideSections() {
     sectionContents.forEach(function(item) {
+        item.classList.add('hide');
+    }) 
+}
+
+// ====================
+// == B. CLIENT LIST ==
+// ====================
+
+
+// ====================
+// == C. ADD CLIENT  ==
+// ====================
+
+
+// =====================
+// == D. TRANSACTIONS ==
+// =====================
+const transactionBtns = [...document.querySelectorAll('.transaction-navBtn')];
+const transactionDepositBtn = transactionBtns[0];
+const transactionWithdrawBtn = transactionBtns[1];
+const transactionTransferBtn = transactionBtns[2];
+
+const transactionSections = [...document.querySelectorAll('.transaction-section')];
+
+// Add click event on transaction buttons to style itself and unhide relevant section
+transactionBtns.forEach(function(item, index) {
+    item.addEventListener('click', function() {
+        clearTransactionBtns();
+        this.classList.add('active-btn');
+
+        const selectedSection = transactionSections[index];
+        hideTransactionSections();
+        selectedSection.classList.remove('hide');
+    })
+});
+
+function clearTransactionBtns() {
+    transactionBtns.forEach(function(item) {
+        item.classList.remove('active-btn');
+    })
+}
+
+function hideTransactionSections() {
+    transactionSections.forEach(function(item) {
         item.classList.add('hide');
     }) 
 }
