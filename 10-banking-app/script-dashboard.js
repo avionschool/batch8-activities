53079425533fc8bad0ef44f320ce5434c2f29805
lea927 <lxthym@icloud.com>
2021-05-24 00:32:01 +0800

    Added function to update sender's balance on Send Money feature

--- 10-banking-app/script-dashboard.js
+++ 10-banking-app/script-dashboard.js
@@ -50,9 +50,10 @@ let btnModalSend = document.getElementById('send-money-btn');
 // sender
 let txtSender = document.getElementById('account-no-from');
 let txtBalSend = document.getElementById('balance-sender');
-let isUsingSend = true; // later on will be used whether to change color of sender's or receiver's textbox to green
+let isUsingSend = true; // later on will be used whether to change color of sender's or receiver's textbox to green or red
 // receiver
 let txtReceiver = document.getElementById('account-no-to');
+let txtBalRec = document.getElementById('balance-receiver');
 // send button
 let btnSendMoney = document.getElementById('btn-send');
 let senderBal; // this later on will be used to check sender's balance is enough
@@ -108,10 +109,9 @@ function searchUser() {
     key = userObj[i];
     if (key.accountNo === txtAcctNo.value) {
       isUserFound = true;
-      FName = key.fullName; // updates value of FName variable to details of the user found
+      FName = key.fullName; // updates value of FName variable to details of the user found, same with AcctNum and bal (global variables that can be re-used)
       AcctNum = key.accountNo;
       bal = parseFloat(key.balance);
-
       return;
     } else {
       isUserFound = false;
@@ -161,7 +161,7 @@ function withdrawMoney() {
   // updates value of variable to re-use update balance function
   txtAmt = document.getElementById('amount_with');
 
-  // displays message if balance is not suffi cient or will update balance
+  // displays message if balance is not sufficient or will update balance
   if (isMoneyEnough == false) {
     alert('Insufficient balance');
     return;
@@ -174,46 +174,48 @@ function withdrawMoney() {
 }
 
 // searches for the sender and receiver's details whichever is applicable
-function sendMoneySearch() {
-  // highlights sender/receiver's textbox when user is found and populates balance textboxes
-  function afterSearch() {
-    if (isUsingSend == true) {
-      if (isUserFound == true) {
-        txtAcctNo.style.backgroundColor = '#81DA77';
-        txtBalSend.value = bal;
-        senderBal = bal;
-      } else {
-        txtAcctNo.style.backgroundColor = '#FAE1E1';
-        txtBalSend.value = '';
-      }
+function searchSenderReceiver() {
+  // additional notes: highlights sender/receiver's textbox when user is found and populates balance textboxes
+  if (isUsingSend == true) {
+    if (isUserFound == true) {
+      txtAcctNo.style.backgroundColor = '#81DA77';
+      txtBalSend.value = bal;
+      senderBal = bal;
     } else {
-      if (isUserFound == true) {
-        txtAcctNo.style.backgroundColor = '#81DA77';
-        txtBalSend.value = bal;
-      } else {
-        txtAcctNo.style.backgroundColor = '#FAE1E1';
-        txtBalSend.value = '';
-      }
+      txtAcctNo.style.backgroundColor = '#FAE1E1';
+      txtBalSend.value = '';
+    }
+  } else {
+    if (isUserFound == true) {
+      txtAcctNo.style.backgroundColor = '#81DA77';
+      txtBalSend.value = bal;
+    } else {
+      txtAcctNo.style.backgroundColor = '#FAE1E1';
+      txtBalSend.value = '';
     }
   }
-
-  afterSearch();
 }
 
 function sendMoney() {
   // error handling: textboxes shouldn't be empty/blank
+  let isFilledOut = false;
   function checkIfFilledOut() {
-    if (txtSender.value === '' || txtReceiver.value === '') {
+    if (txtBalRec.value === '' || txtBalSend.value === '') {
       alert('Please fill-out missing details. Transaction failed.');
+      isFilledOut = false;
       return;
+    } else {
+      isFilledOut = true;
     }
   }
 
   // error handling: checks if sender and receiver's the same user
   function areUsersSame() {
-    if (txtSender.value === txtReceiver.value) {
-      alert('Sender and receiver are the same. Transaction failed.');
-      return;
+    if (isFilledOut == true) {
+      if (txtSender.value === txtReceiver.value) {
+        alert('Sender and receiver are the same. Transaction failed.');
+        return;
+      }
     }
   }
 
@@ -225,10 +227,25 @@ function sendMoney() {
     }
   }
 
+  // update sender's balance once validations have been passed
+  function updateRunBalSend() {
+    // re-assigns to original element to be used in the computation below
+    txtBalSend = document.getElementById('balance-sender');
+    // gets value of sender's account no. to be passed on to update balance function
+    AcctNum = document.getElementById('account-no-from').value;
+    // running bal will be passed on to update balance function
+    runningBal = parseFloat(txtBalSend.value) - parseFloat(txtAmtSend.value);
+    // console.log(runningBal);
+
+    updateBalance();
+    // alert(`Transaction successful! \n Account No. ${AcctNum} \n Running balance: ${runningBal}`);
+  }
+
   // invoke functions here
   checkIfFilledOut();
   areUsersSame();
   checkSenderBalance();
+  updateRunBalSend();
 }
 
 // ===============================
@@ -334,6 +351,10 @@ btnModalSend.addEventListener('click', () => {
   window.onclick = function (e) {
     e.target == sendModal ? (sendModal.style.display = 'none') : null;
   };
+
+  // disables sender's and receiver's balance
+  txtBalRec.disabled = true;
+  txtBalSend.disabled = true;
 });
 
 // triggers function to search for sender's details
@@ -346,7 +367,7 @@ txtSender.addEventListener('keyup', (e) => {
   txtBalSend = document.getElementById('balance-sender'); // re-assigned to original element since the function on the receiver txtbox will change it to another element
   if (e.code === 'Enter') {
     searchUser();
-    sendMoneySearch();
+    searchSenderReceiver();
   }
 });
 
@@ -360,7 +381,7 @@ txtReceiver.addEventListener('keyup', (e) => {
   isUsingSend = false;
   if (e.code === 'Enter') {
     searchUser();
-    sendMoneySearch();
+    searchSenderReceiver();
   }
 });
 
