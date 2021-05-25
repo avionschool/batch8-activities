9e2312dd42109c4ce466eeec4c485453fca622c4
Ma Erika Santos <sntserika22@gmail.com>
2021-05-25 20:36:28 +0800

    edits

--- Banking App/main.js
+++ Banking App/main.js
@@ -2,9 +2,9 @@ var id, password, infoname, balance, typemoney, rank, joindate, country  = new A
 var account = new Array();
 var amount_history = new Array();
 var transactions = new Array;
-id= [1, 2];
-password= [11,22];
-infoname = ["John Doe","Jane Doe"];
+id = [1, 2];
+password= [11, 22];
+infoname = ["Adam Hill","Billy Doe"];
 balance = [0,0,0,0];
 // typemoney =["Peso Savings","Peso Savings"];
 // rank = ["Gold","Silver","Copper","New"];
@@ -14,7 +14,7 @@ for (i = 0; i < id.length; i++){
 	account[i] = parseInt(balance[i]);
 }
 var i = 0;
-var success = -1;
+var loginsuccess = -1;
 
 // login page
 function Login(){
@@ -25,8 +25,8 @@ function Login(){
             document.getElementById("content").style.display = 'block';
             document.getElementById("content2").style.display = 'block';
             document.getElementById("content3").style.display = 'block';
-                alert("Success Login, Welcome " + infoname[i]);
-        		success=i;
+                alert("Successful login. Welcome " + infoname[i] + "!");
+        		loginsuccess=i;
         		prjoindate=joindate[i];
         		prname=infoname[i];
 				prbalance=account[i];
@@ -39,13 +39,14 @@ function Login(){
 	document.getElementById("balance").innerHTML = prbalance;
 	// document.getElementById("typemoney").innerHTML = prtypemoney;
 	// document.getElementById("country").innerHTML =prcountry;
-        }
+		}
+        // }else if (document.getElementById("idname").value != id && document.getElementById("pass").value != password) {
+		// 	alert("The username or password is incorrect, please try again.");
+		if (loginsuccess == -1) {
+		  alert("The username or password is incorrect, please try again.");
+		}
 	}
-	if (success == -1) {
-        alert("The username or password is incorrect, please try again.");
-    }
 }
-
 
 function Deposit() {
 	document.getElementById("errortick").style.display = 'none';
@@ -59,12 +60,13 @@ function Deposit() {
     	document.getElementById("notick").style.display = 'none';
     }else{
 	input_amount = parseInt(input_amount);
-    account[success] += input_amount;
-    prbalance=account[success];
+    account[loginsuccess] += input_amount;
+    prbalance=account[loginsuccess];
     document.getElementById("balance").innerHTML = prbalance; 
     document.getElementById("notification") .innerHTML= "Success! ₱ "+input_amount+" has been credited on your account.";
 	amount_history.push(document.getElementById("Depositinput").value);
 	transactions.push("Deposit");
+	// to display on transactions history
 	for (i = 0; i < amount_history.length; i++) {
     document.getElementById("amt_history").innerHTML = amount_history;
     document.getElementById("acct_description").innerHTML = transactions;
@@ -86,27 +88,28 @@ function Withdrawal() {
     	 document.getElementById("notick").style.display = 'none';
     	 document.getElementById("errortick").style.display = 'block';
         document.getElementById("notification").innerHTML = "Error. You entered the wrong format. Please enter a number ";
-    } else if (account[success] - parseInt(input_amount) >= 1) {
+    } else if (account[loginsuccess] - parseInt(input_amount) >= 1) {
     	document.getElementById("errortick").style.display = 'none';
-        account[success] -= parseInt(input_amount);
+        account[loginsuccess] -= parseInt(input_amount);
         document.getElementById("notification").innerHTML = "Success! Your account has been debited ₱ " + input_amount + ".";
-        prbalance=account[success];
+        prbalance=account[loginsuccess];
         document.getElementById("balance").innerHTML =prbalance;
 		amount_history.push(document.getElementById("Withdrawalinput").value);
 	  	transactions.push("Withdraw");
+		// to display on transactions history
 	 	for (i = 0; i < amount_history.length; i++) {
         document.getElementById("amt_history").innerHTML = amount_history;
         document.getElementById("acct_description").innerHTML = transactions;
 		document.getElementById("Withdrawalinput").value = '';
 		 }
-    } else if (account[success] - parseInt(input_amount) < 0) {
+    } else if (account[loginsuccess] - parseInt(input_amount) < 0) {
     	document.getElementById("notick").style.display = 'none';
     	document.getElementById("errortick").style.display = 'block';
         document.getElementById("notification").innerHTML = "Transaction declined. Insufficient balance.";
     } else {
     	document.getElementById("notick").style.display = 'none';
     	document.getElementById("errortick").style.display = 'block';
-        document.getElementById("notification").innerHTML = "Error. Your balance is ₱ " + account[success] + ".";
+        document.getElementById("notification").innerHTML = "Error. Your balance is ₱ " + account[loginsuccess] + ".";
     }
 }
 
@@ -128,10 +131,10 @@ function Transfers(){
 	var input_acctNum = document.getElementById("accountnumber").value;
 	var k=0;
 	for (i = 0; i < id.length; i++){		
-	if ((input_acctNum == id[i] && inputmoney>0 && account[success]-inputmoney>0)){
+	if ((input_acctNum == id[i] && inputmoney>0 && account[loginsuccess]-inputmoney>0)){
 		k=1;
-		account[success] -= inputmoney;
-		prbalance=account[success];
+		account[loginsuccess] -= inputmoney;
+		prbalance=account[loginsuccess];
 		account[i] += inputmoney;
 		document.getElementById("errortick").style.display = 'none';
 		document.getElementById("balance").innerHTML = prbalance;
@@ -160,20 +163,20 @@ function Change() {
     document.getElementById("errortick").style.display = 'none';
 }
 function Changepassword(){
-	if (document.getElementById("passcurrent").value == password[success] && document.getElementById("passnew").value == document.getElementById("passnewrepeat").value) {
-        password[success] = document.getElementById("passnew").value;
+	if (document.getElementById("passcurrent").value == password[loginsuccess] && document.getElementById("passnew").value == document.getElementById("passnewrepeat").value) {
+        password[loginsuccess] = document.getElementById("passnew").value;
         document.getElementById("changepass").style.display = 'none';
 	document.getElementById("changeimg").style.display = 'none';
 	document.getElementById("no").style.display = 'block';
 	document.getElementById("notick").style.display = 'block';
-	document.getElementById("notification") .innerHTML= "Success. You password has been change";
+	document.getElementById("notification") .innerHTML= "Success! You password has been changed.";
     } else {
     	document.getElementById("changepass").style.display = 'none';
 	document.getElementById("changeimg").style.display = 'none';
 	document.getElementById("no").style.display = 'block';
 	document.getElementById("notick").style.display = 'none';
 	document.getElementById("errortick").style.display = 'block';
-        document.getElementById("notification") .innerHTML= "Error. Password wrong ";
+        document.getElementById("notification") .innerHTML= "Error. Wrong Password. ";
     }
 }
 
