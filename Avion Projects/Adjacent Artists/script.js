d37ec88f732e3f93ea21d64ec7ff39811d90aecf
theIanMilan <theIanMilan@users.noreply.github.com>
2021-06-10 20:22:11 +0800

    Finish API script generation

--- Avion Projects/Adjacent Artists/script.js
+++ Avion Projects/Adjacent Artists/script.js
@@ -58,18 +58,53 @@ let test = {
 
 // TASTEDIVE
 const TASTEDIVE_KEY = `416207-Adjacent-53G7VTDP`;
-let artistString = "Paramore";
-let requestURL_tastedive = `https://tastedive.com/api/similar?q=${artistString}&k=${TASTEDIVE_KEY}&limit=5&type=music&verbose=1&callback=myDisplayFunction`;
+var artistString = "";
 var results; // initialize variable to contain JSON objects
+
+// MUSIC DOM
+const inputMusic = document.querySelector('#search-music'); 
+const cards = document.querySelector('.cards');
+
+inputMusic.addEventListener('keydown', function(e) {
+    if (e.key === "Enter") {
+        artistString = inputMusic.value; // change query string
+        generateSimilarArtistsScript(); // generate script
+    }
+});
 
 // Functions
 async function generateSimilarArtistsScript() {
     let s = document.createElement("script");
-    s.src = requestURL_tastedive;
+    s.src = `https://tastedive.com/api/similar?q=${artistString}&k=${TASTEDIVE_KEY}&limit=5&type=music&verbose=1&callback=getSimilarArtists`;
     document.body.appendChild(s);
 }
 
-function myDisplayFunction(myObj) {
-  results = myObj; // it works!!!
-  document.getElementById("demo").innerHTML = results.Similar.Results[0].Name;
+function getSimilarArtists(responseObject) {
+    results = responseObject; // store in global
+    createCards(results);
 } 
+
+function createCards(obj) {
+    cards.innerHTML = ""; // Reset
+    let section = "";
+
+    for (let i = 0; i < obj.Similar.Results.length; i++) {
+        let ARTIST_NAME = obj.Similar.Results[i].Name;
+        let DESCRIPTION = obj.Similar.Results[i].wTeaser;
+        let WIKI_LINK = obj.Similar.Results[i].wUrl;
+        let YOUTUBE_LINK = obj.Similar.Results[i].yUrl;
+
+        section += `        
+        <div class="results-container">
+            <div class="results-name"><a href="${WIKI_LINK}" target="_blank">${ARTIST_NAME}</a></div>
+            <div class="results-video-container">
+                <iframe class="results-video" width="420" height="315"
+                src="${YOUTUBE_LINK}">
+                </iframe>
+            </div>
+            <div class="results-snippet truncate-overflow">${DESCRIPTION}</div>
+        </div>`
+    }
+
+    cards.innerHTML = section;
+}
