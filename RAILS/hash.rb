280055423612eb86b78c4c69a1c403808ff860ae
Ma Erika Santos <sntserika22@gmail.com>
2021-06-25 21:01:44 +0800

    learning rb

---
+++ RAILS/hash.rb
@@ -0,0 +1,4 @@
+contact_data = [["ana@email.com", "123 Main st.", "555-123-4567"],
+            ["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Analyn Cajocson" => {}, "Avion  School" => {}}
---
+++ RAILS/square_of_squares.rb
@@ -0,0 +1,3 @@
+puts "Enter a number: "
+num = gets.chomp.to_i
+puts (Math.pow(num,0.5))
