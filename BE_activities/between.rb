34e2095ced7a67837396352e8ccf8f7d97e9f36e
Edwin Sacalamitao <edwin@avionschool.com>
2021-06-26 09:22:51 +0800

    final

--- BE_activities/between.rb
+++ BE_activities/between.rb
@@ -1,12 +1,12 @@
 def check_num (a)
     if a >= 1 && a <= 50
-      puts "1-50 range"
+      puts "number is between 1-50 range"
     elsif a >= 51 && a <= 100
-      puts "51-100 range"
+      puts "number is between  51-100 range"
     else
-      puts "Over 100"
+      puts "number is between Over 100"
     end
-  end
+  endcd 
   
   print check_num(12),"\n"
   print check_num(58),"\n"
