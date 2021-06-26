13b692117f141baebee76051c2cdb4699ac03550
Ma Erika Santos <sntserika22@gmail.com>
2021-06-26 19:10:00 +0800

    edits

--- RAILS/2.0_1_missing_code.rb
+++ RAILS/2.0_1_missing_code.rb
@@ -1,21 +1,22 @@
 class Profile
-  attr_writer :full_name, :age, :address, :work
-  attr_reader :full_name, :age, :address, :work
+# getter and setter    
+attr_accessor :full_name, :age, :address, :work
     def initialize(full_name, age, address, work)
-      @full_name = full_name
-      @age = age
-      @address = address
-      @work = work
+        @full_name = full_name
+        @age = age
+        @address = address
+        @work = work
     end
 end
+        
 my_profile = Profile.new('Juan', 18, 'Bulacan', 'Instructor')
-    
+        
 puts my_profile.full_name
-my_profile.full_name = 'Juan Cruz'
-    
-my_profile.age = 25
-my_profile.work = 'Software Engineer'
-    
+ my_profile.full_name = 'Juan Cruz'
+        
+ my_profile.age = 25
+ my_profile.work = 'Software Engineer'
+# output the attributes    
 puts my_profile.full_name
 puts my_profile.age
 puts my_profile.work
--- RAILS/between.rb
+++ RAILS/between.rb
@@ -4,9 +4,9 @@ number = gets.chomp.to_i
 if number < 0
   puts "You can't enter a negative number!"
 elsif number <= 50
-  puts "#{number} is between 0 and 50."
+  puts "#{number} is between -1 and 50."
 elsif number <= 100
-  puts "#{number} is between 51 and 100."
+  puts "#{number} is between 50 and 100."
 else
-  puts "#{number} is above 100. Number should be between 0 and 100."
+  puts "#{number} is above 99. Number should be between 0 and 100."
 end
