9dc46e4e1fe2faab40f580ccc48c92c2d81b036a
lea927 <lxthym@icloud.com>
2021-06-28 16:50:28 +0800

    Added stop exercise

---
+++ ruby-activities/stop.rb
@@ -0,0 +1,12 @@
+prompt = "Enter a random word or the word 'stop': "
+puts prompt
+
+while input = gets.chomp.upcase
+  case input
+  when "STOP"
+    puts "Bye! Program will exit now."
+    break
+  else
+    puts prompt
+  end
+end
