7b6c57ce56fd20ba63bac2ab54d2e7a335eec854
lea927 <lxthym@icloud.com>
2021-06-29 19:43:29 +0800

    Added ruby file for count of positives coding exercises

---
+++ ruby-activities/count_positives.rb
@@ -0,0 +1,19 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15] 
+arr_2 = []
+positive_counter = 0
+sum_of_negatives = 0
+
+if arr.length == 0
+  puts arr_2
+else
+  arr.each { |num|
+    if num > 0
+      positive_counter += 1 
+    elsif num < 0
+      sum_of_negatives += num
+    end
+  }
+  arr_2.push(positive_counter)
+  arr_2.push(sum_of_negatives)
+  puts arr_2
+end
