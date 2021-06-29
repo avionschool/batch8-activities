2bce437693fe8299d30b6517965e803ade337daf
Janson Siy <jansonharold.siy@gmail.com>
2021-06-29 18:45:17 +0800

    Updated

--- backend_projects/class_confection_activity.rb
+++ backend_projects/class_confection_activity.rb
@@ -2,39 +2,67 @@ class Confection
     def ingredients
       puts "Flour, Sugar, Baking Powder, Salt, Milk, Vegtable Oil, Eggs, Water"
     end
-  end
+end
   
-  class Cupcake < Confection
+class Cupcake < Confection
     def bake
-      puts "Baking at 350 degrees for 25 minutes"
+        puts "Baking at 350 degrees for 25 minutes"
     end
     def frost
-      puts "Apply frosting"
+        puts "Apply frosting"
     end
     def success
-      puts "Cupcake made!"
+        puts "Cupcake made!"
     end
-  end
+end
   
-  class BananaCake < Confection
+class BananaCake < Confection
     def bake
-      puts "Baking at 350 degrees for 25 minutes."
+        puts "Baking at 350 degrees for 25 minutes."
     end
     def success
-      puts "Banana Cake made!"
+        puts "Banana Cake made!"
     end
-  end
+end
   
-  confection = Confection.new()
-  confection.ingredients
+confection = Confection.new()
+confection.ingredients
+
+cupcake = Cupcake.new()
+cupcake.ingredients
+cupcake.bake
+cupcake.frost
+cupcake.success
+
+bananaCake = BananaCake.new()
+bananaCake.ingredients
+bananaCake.bake
+bananaCake.success
+
+# class Jan
+#     def say_hello
+#         puts "Hello"
+#     end
+#     def say_hi
+#         puts "Hi"
+#     end
+#     def introduce
+#         puts "I'm Jan"
+#     end
+# end
   
-  cupcake = Cupcake.new()
-  cupcake.ingredients
-  cupcake.bake
-  cupcake.frost
-  cupcake.success
+# class Jeff < Jan
+#     def introduce
+#         puts "I'm Jeff"
+#     end
+# end
   
-  bananaCake = BananaCake.new()
-  bananaCake.ingredients
-  bananaCake.bake
-  bananaCake.success
+# greet = Jan.new()
+# greet.say_hello
+# greet.say_hi
+# greet.introduce
+
+# greetToo = Jeff.new()
+# greetToo.say_hello
+# greetToo.say_hi
+# greetToo.introduce  
