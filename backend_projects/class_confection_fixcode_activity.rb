62b2e31d7b60b4945787da9b5c88c69c67dbf18b
Janson Siy <jansonharold.siy@gmail.com>
2021-06-29 19:12:11 +0800

    Updated

--- backend_projects/class_confection_fixcode_activity.rb
+++ backend_projects/class_confection_fixcode_activity.rb
@@ -65,4 +65,54 @@ bananaCake.success
 # greetToo = Jeff.new()
 # greetToo.say_hello
 # greetToo.say_hi
-# greetToo.introduce  
+# greetToo.introduce
+
+module AdminPermisson
+    def edit_users_profile
+      puts "Admin updated all users profile"
+    end
+end
+  
+module BuyerPermission
+    def buy
+      puts "Buyer has bought an item"
+    end
+end
+  
+class User
+    def initialize(username, password, ip_address)
+        @username = username
+        @password = password
+        @ip_address = ip_address
+    end
+end
+  
+    protected
+    def login
+      puts "User logged in. IP address: #@{ip_address}"
+    end
+end
+  
+class Admin < User
+    def admin_login
+        puts "Admin logged in"
+    end
+end
+  
+  
+class Buyer < User
+end
+
+## execute
+
+my_admin = Admin.new('avionuser', 'password', '127.0.0.1')
+my_admin.admin_login
+my_admin.edit_users_profile
+
+my_admin.change_password = 'new_password'
+
+buyer = Buyer.new('juan', 'password', '127.0.0.1')
+buyer.buyer_login
+buyer.buy
+
+buyer.change_password = 'new_password'
