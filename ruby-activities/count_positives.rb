c0175bca74663b660af8a4a942f028d22dbfd81b
lea927 <lxthym@icloud.com>
2021-06-29 20:02:00 +0800

    Updated file to return a one-liner array

--- ruby-activities/count_positives.rb
+++ ruby-activities/count_positives.rb
@@ -1,10 +1,11 @@
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15] 
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15] 
+arr = []
 arr_2 = []
 positive_counter = 0
 sum_of_negatives = 0
 
 if arr.length == 0
-  puts arr_2
+  p []
 else
   arr.each { |num|
     if num > 0
@@ -15,5 +16,5 @@ else
   }
   arr_2.push(positive_counter)
   arr_2.push(sum_of_negatives)
-  puts arr_2
+  p arr_2
 end
